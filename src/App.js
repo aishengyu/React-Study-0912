f2da55d50872983394b93bb90cfe1014a3fda19b
aishengyu <134182371+aishengyu@users.noreply.github.com>
2023-09-12 05:26:43 +0000

    {faceShowFlag && <p>😊😊😊😊</p>} faceShowFlag || setFaceShowFlag(true); faceShowFlag && setFaceShowFlag(false); useEffect(() => {});の使用例

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+// eslint-disable-next-line react-hooks/exhaustive-deps
+
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
+  console.log("最初");
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickIncrement = () => {
+    setNum(num + 1);
+    console.log(num);
+  };
+
+  const onClickSwitchFaceShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  /* 初心者が良く起こるミス
+  if (num % 3 === 0) {
+    setFaceShowFlag(true);
+  } else {
+    setFaceShowFlag(false);
+  }
+  */
+  useEffect(() => {
+    console.log("useEffect!!!");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // }, []); [] 一回だけ実行します。
+  }, [num]); // [num] numが変わった時だけ実行されます。
+
   return (
     <>
       <h1 style={{ color: "red" }}> こんにちは </h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickIncrement}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchFaceShowFlag}> On/Off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>😊😊😊😊</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const ColorfulMessage = (props) => {
   //console.log(props);
+  console.log("カラフル");
   const contentStyle = {
     color: props.color,
     fontSize: "20px"

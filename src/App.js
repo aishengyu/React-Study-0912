42bde657576787176a8a28925629d450a7e55690
aishengyu <134182371+aishengyu@users.noreply.github.com>
2023-09-12 02:58:35 +0000

    {props.message}の代わりに{props.children}を使います。

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import ColorfulMessage from "./components/ColorfulMessage";
 const App = () => {
   const onClickButton = () => alert();
   return (
-    //<React.Fragment>
     <>
       <h1 style={{ color: "red" }}> こんにちは </h1>
-      <ColorfulMessage color="blue" message="お元気ですか？" />
-      <ColorfulMessage color="pink" message="元気です" />
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickButton}>登録</button>
     </>
-    //</React.Fragment>
   );
 };
 
